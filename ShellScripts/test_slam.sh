40205d14ea6348142b0baba78d7deecbc7b7012e
Boris Dayma <boris.dayma@gmail.com>
2018-07-21 17:34:27 -0700

    feat: allow custom home directory in shell scripts

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-xterm  -e  " source $HOME/catkin_ws/devel/setup.bash; roscore" & 
+
+# Setup DIR to home directory if necessary (useful for Udacity workspace)
+DIR=$HOME
+
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roscore" & 
 sleep 5
-xterm  -e  " source $HOME/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/World/ushape.world" &
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$DIR/catkin_ws/src/home_service_robot/World/ushape.world" &
 sleep 5
-xterm  -e  " source $HOME/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" &
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" &
 sleep 5
-xterm  -e  " source $HOME/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm  -e  " source $HOME/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
