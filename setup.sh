a9599611bd7db964617c3f7f826cba183f787bc4
Boris Dayma <boris.dayma@gmail.com>
2018-07-22 21:47:37 -0700

    feat: added setup script to initialize environment

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# initialize environment to run project
+
+# install required packages
+sudo apt-get update
+sudo apt-get install ros-kinetic-navigation
+
+# create catkin workspace
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+git clone https://github.com/borisd13/home_service_robot
+cd home_service_robot
+git submodule init
+git submodule update
+cd ../..
+
+# build catkin workspace
+catkin_make
