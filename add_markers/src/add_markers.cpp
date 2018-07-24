774fbe27ee758170dbb0d97d497310817923ea94
Boris Dayma <boris.dayma@gmail.com>
2018-07-23 19:34:12 -0700

    feat: add_markers and pick_objects node communicate together
    pick_objects sends a message used by add_markers when reaching target
    locations

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,19 +1,54 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include "std_msgs/String.h"
+
+// Define a subscriber to receive messages when reaching pick-up and drop-off locations
+ros::Subscriber target_sub;
+
+// Define a publisher for markers on rviz
+ros::Publisher marker_pub;
+
+// Define the marker to publish
+visualization_msgs::Marker marker;
+
+// Callback when a message is received
+void target_callback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("Received message: [%s]", msg->data.c_str());
+
+  if (msg->data == "pick-up")
+  {
+    // Object has been picked up and we need to hide it
+    marker.action = visualization_msgs::Marker::DELETE;
+    marker_pub.publish(marker);
+  }
+  if (msg->data == "drop-off")
+  {
+    // Object can be displayed at drop-off location
+    marker.action = visualization_msgs::Marker::ADD;
+    // TODO target locations should be passed by other node or as parameters
+    marker.pose.position.x = 1.6;
+    marker.pose.position.y = 7.6;
+    marker.pose.orientation.z = 1.;
+    marker.pose.orientation.w = 0.74;
+    marker_pub.publish(marker);
+  }
+}
 
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  target_sub = n.subscribe("target_msg", 10, target_callback);
 
-  // Set our initial shape type to be a cube
+  // Set our shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
   while (ros::ok())
   {
-    visualization_msgs::Marker marker;
+    
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time::now();
@@ -30,15 +65,16 @@ int main( int argc, char** argv )
     marker.action = visualization_msgs::Marker::ADD;
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    // TODO target locations should be passed by other node or as parameters
     marker.pose.position.x = 7.6;
     marker.pose.position.y = 1.1;
     marker.pose.orientation.z = 0.67;
     marker.pose.orientation.w = 0.74;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 0.5;
+    marker.scale.y = 0.5;
+    marker.scale.z = 0.5;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
@@ -60,6 +96,7 @@ int main( int argc, char** argv )
     }
     marker_pub.publish(marker);
 
-    r.sleep();
+    // Spin to wait for messages (reaching target locations)
+    ros::spin();
   }
 }
--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -1,13 +1,21 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include "std_msgs/String.h"
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+// Define a publisher to communicate when reaching pick-up and drop-off locations
+ros::Publisher target_pub;
+
 int main(int argc, char** argv){
   // Initialize the pick_objects node
   ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
+
+  // Initialize publisher
+  target_pub = n.advertise<std_msgs::String>("target_msg", 10);
 
   // tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -18,6 +26,7 @@ int main(int argc, char** argv){
   }
 
   move_base_msgs::MoveBaseGoal goal_1, goal_2;
+  std_msgs::String msg;
 
   // set up the frame parameters
   goal_1.target_pose.header.frame_id = "map";
@@ -38,7 +47,11 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
     ROS_INFO("Reached pick-up location! Picking up object...");
+    msg.data = "pick-up";
+    target_pub.publish(msg);
+  }
   else
   {
     ROS_INFO("Failed to reach pick-up location");
@@ -71,7 +84,11 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
     ROS_INFO("Reached drop-off location!");
+    msg.data = "drop-off";
+    target_pub.publish(msg);
+  }
   else
     ROS_INFO("Failed to reach drop-off location");
 
