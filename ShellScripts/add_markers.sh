ed3bdc7df4c005dc16e3b9d553559d6664191d87
Boris Dayma <boris.dayma@gmail.com>
2018-07-23 18:17:51 -0700

    feat: create a add_markers node
    This node is used to display markers in rviz at specific locations

---
+++ ShellScripts/add_markers.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Setup DIR to home directory if necessary (useful for Udacity workspace)
+DIR=$HOME
+
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roscore" & 
+sleep 5
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$DIR/catkin_ws/src/home_service_robot/World/ushape.world" &
+sleep 5
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; rosservice call /gazebo/set_model_state '{model_state: { model_name: mobile_base, pose: { position: { x: -2.7, y: 7.3 ,z: 0 }, orientation: {x: 0, y: 0, z: -0.676, w: 0.737}}}}'" &
+sleep 5
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$DIR/catkin_ws/src/home_service_robot/World/my_map.yaml" &
+sleep 5
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " source $DIR/catkin_ws/devel/setup.bash; rosrun add_markers add_markers_node" &
