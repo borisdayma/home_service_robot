4ece563ef8fcd1c719bc245d1da051be6804703e
Boris Dayma <boris.dayma@gmail.com>
2018-07-26 19:33:29 -0500

    fix: edit start position in test_slam script
    All scripts rely on a specific starting position that is now correctly
    set in test_slam.

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -6,6 +6,8 @@ xterm  -e  " source $CATKIN_DIR/devel/setup.bash; roscore" &
 sleep 5
 xterm  -e  " source $CATKIN_DIR/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find wall_follower)/../World/ushape.world" &
 sleep 5
+xterm  -e  " source $CATKIN_DIR/devel/setup.bash; rosservice call /gazebo/set_model_state '{model_state: { model_name: mobile_base, pose: { position: { x: -2.7, y: 7.3 ,z: 0 }, orientation: {x: 0, y: 0, z: -0.676, w: 0.737}}}}'" &
+sleep 5
 xterm  -e  " source $CATKIN_DIR/devel/setup.bash; rosrun gmapping slam_gmapping" &
 sleep 5
 xterm  -e  " source $CATKIN_DIR/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
