05580bd7d7a50cb2d16f14941b223d3966bb6eae
Boris Dayma <boris.dayma@gmail.com>
2018-07-26 19:57:11 -0500

    fix: update gmapping parameters in test_slam script
    Parameters updated to perform a better mapping as per wall_follower

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -8,7 +8,7 @@ xterm  -e  " source $CATKIN_DIR/devel/setup.bash; roslaunch turtlebot_gazebo tur
 sleep 5
 xterm  -e  " source $CATKIN_DIR/devel/setup.bash; rosservice call /gazebo/set_model_state '{model_state: { model_name: mobile_base, pose: { position: { x: -2.7, y: 7.3 ,z: 0 }, orientation: {x: 0, y: 0, z: -0.676, w: 0.737}}}}'" &
 sleep 5
-xterm  -e  " source $CATKIN_DIR/devel/setup.bash; rosrun gmapping slam_gmapping" &
+xterm  -e  " source $CATKIN_DIR/devel/setup.bash; rosrun gmapping slam_gmapping _linearUpdate:=0.1 _angularUpdate:=0.1 _particles:=30 _xmin:=-20 _ymin:=-20 _xmax:=20 _ymax:=20 _map_update_interval:=0.5 _lskip:=1 _delta:=0.05 _minimumScore:=200 _maxUrange:=50" &
 sleep 5
 xterm  -e  " source $CATKIN_DIR/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
